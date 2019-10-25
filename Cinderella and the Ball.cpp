d3e1683d73db0bd217e6abb2c8642ce179252f25
Krishnan S G <43802499+KrishnanSG@users.noreply.github.com>
2019-10-25 09:08:06 +0530

    Add files via upload

---
+++ Cinderella and the Ball.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int gcd(int, int);
+int main()
+{
+    int n, m, q, i;
+    cin>>n>>m>>q;
+    int cx[q], cy[q], px[q], py[q];
+    for(i=0;i<q;i++)
+        cin>>cx[i]>>cy[i]>>px[i]>>py[i];
+    int groupc, groupp, g;
+    g=gcd(n,m);
+    for(i=0;i<q;i++)
+    {
+        groupc  = ceil(cy[i]*g/cx[i]);
+        groupp  = ceil(py[i]*g/px[i]);
+        if(groupc==groupp)
+            cout<<"YES";
+        else
+            cout<<"NO";
+        cout<<endl;
+    }
+}
+
+int gcd(int n, int m)
+{
+    if (n == 0)
+       return m;
+    if (m == 0)
+       return n;
+    if (n == m)
+        return n;
+    if (n > m)
+        return gcd(n-m, m);
+    return gcd(n, n-m);
+}
